084af411a1d5e5da3a3bdb4487f7354ac7779910
Asif Ul Alam <asifalam0308@gmail.com>
2023-06-29 20:09:07 +1000

    scraping

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import { ChatGPTClient } from './clients/chatgpt.client';
+import { ScraperClient } from "./clients/scraper.client"
 
-const chatgpt = new ChatGPTClient()
-chatgpt.completeWithErrorHandling("Say hello")
-  .then(r => console.log(r?.data?.choices[0]?.text))
-  .catch(e => console.log(e))
+const url =
+  "https://www.enr.com/articles/56337-southwest-design-firms-report-robust-revenue-gains-in-2022"
+
+const scraper = new ScraperClient()
+
+const selector = ".main-body p"
+scraper
+  .getText(url, selector)
+  .then((e) => console.log((e)))
+  .catch((e) => console.log(e))
